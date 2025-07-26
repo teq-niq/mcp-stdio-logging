236252bae35fa62956bbe1ac02582a60c6b112d1
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2025-07-26 20:11:57 +0200

    final variables/ formatting code

--- mymcpserver/src/main/java/com/eg/mcp/mytransport/MyStdioServerTransportProvider.java
+++ mymcpserver/src/main/java/com/eg/mcp/mytransport/MyStdioServerTransportProvider.java
@@ -128,10 +128,10 @@ public class MyStdioServerTransportProvider implements McpServerTransportProvide
 		private final AtomicBoolean isStarted = new AtomicBoolean(false);
 
 		/** Scheduler for handling inbound messages */
-		private Scheduler inboundScheduler;
+		private final Scheduler inboundScheduler;
 
 		/** Scheduler for handling outbound messages */
-		private Scheduler outboundScheduler;
+		private final Scheduler outboundScheduler;
 
 		private final Sinks.One<Void> outboundReady = Sinks.one();
 
@@ -202,7 +202,7 @@ public class MyStdioServerTransportProvider implements McpServerTransportProvide
 			if (isStarted.compareAndSet(false, true)) {
 				this.inboundScheduler.schedule(() -> {
 					inboundReady.tryEmitValue(null);
-					BufferedReader reader = null;
+					BufferedReader reader;
 					try {
 						reader = new BufferedReader(new InputStreamReader(inputStream));
 						while (!isClosing.get()) {
