79fedf40633e105017b9243f7bede77468e924f0
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2025-07-26 20:13:52 +0200

    remove blank spaces / formatting code / unnecessary semicolon

--- mymcpserver/src/main/java/com/eg/mcp/providers/others/StoreMcpPromptProvider.java
+++ mymcpserver/src/main/java/com/eg/mcp/providers/others/StoreMcpPromptProvider.java
@@ -3,23 +3,23 @@ package com.eg.mcp.providers.others;
 import java.util.List;
 import java.util.random.RandomGenerator;
 
-import org.springframework.stereotype.Service;
-
 import com.eg.mcp.utils.CountryPromptDatabase;
 import com.logaritex.mcp.annotation.McpArg;
 import com.logaritex.mcp.annotation.McpPrompt;
-
 import io.modelcontextprotocol.spec.McpSchema.GetPromptResult;
 import io.modelcontextprotocol.spec.McpSchema.PromptMessage;
 import io.modelcontextprotocol.spec.McpSchema.Role;
 import io.modelcontextprotocol.spec.McpSchema.TextContent;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public class StoreMcpPromptProvider {
-	
-	private CountryPromptDatabase countryPromptDatabase;
+
+	private final CountryPromptDatabase countryPromptDatabase;
+
 	private static final RandomGenerator RNG = RandomGenerator.of("L64X128MixRandom");
-	
+
 	public StoreMcpPromptProvider(CountryPromptDatabase countryPromptDatabase) {
 		super();
 		this.countryPromptDatabase = countryPromptDatabase;
@@ -27,40 +27,35 @@ public class StoreMcpPromptProvider {
 
 	@McpPrompt(name = "brandz-greeting", description = "Greets the user visiting Brand Z Sports Store")
 	public GetPromptResult brandzGreetingPrompt(
-	        @McpArg(name = "name", description = "The name of the user", required = true) String name) {
-	    
-	    String message = "Hi " + name + "! 👋 Welcome to Brand Z Sports Store.";
-	            
+			@McpArg(name = "name", description = "The name of the user", required = true) String name) {
 
-	    return new GetPromptResult("Brand Z Greeting",
-	            List.of(new PromptMessage(Role.ASSISTANT, new TextContent(message))));
+		String message = "Hi " + name + "! 👋 Welcome to Brand Z Sports Store.";
+
+
+		return new GetPromptResult("Brand Z Greeting",
+				List.of(new PromptMessage(Role.ASSISTANT, new TextContent(message))));
 	}
-	
+
 	@McpPrompt(name = "country-status", description = "Gives information on how many stores are there in the input country name")
 	public GetPromptResult countryStoreStatus(
-	        @McpArg(name = "countryName", description = "The name of the country", required = true) String countryName) {
-		String message=null;
+			@McpArg(name = "countryName", description = "The name of the country", required = true) String countryName) {
+		String message;
 		if (countryName == null || countryName.isBlank()) {
-    	    message="Enter a country";
-    	}
-		else
-		{
-			if(countryPromptDatabase.getCountries().contains(countryName))
-			{
-				
+			message = "Enter a country";
+		}
+		else {
+			if (countryPromptDatabase.getCountries().contains(countryName)) {
 				int randomInt = RNG.nextInt(10); // [0, 100)
-				message=countryName+ " has "+randomInt+" stores";;
+				message = countryName + " has " + randomInt + " stores";
 			}
-			else
-			{
-				message="Enter a valid country";
+			else {
+				message = "Enter a valid country";
 			}
 		}
-	    
-	            
 
-	    return new GetPromptResult("Number of stores in the country",
-	            List.of(new PromptMessage(Role.ASSISTANT, new TextContent(message))));
+
+		return new GetPromptResult("Number of stores in the country",
+				List.of(new PromptMessage(Role.ASSISTANT, new TextContent(message))));
 	}
 
 }
