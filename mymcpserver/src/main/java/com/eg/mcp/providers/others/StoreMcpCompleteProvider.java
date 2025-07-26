863a2d7eb18f96426406da796b87c03b4fc2afd8
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2025-07-26 20:12:44 +0200

    remove blank spaces / formatting code

--- mymcpserver/src/main/java/com/eg/mcp/providers/others/StoreMcpCompleteProvider.java
+++ mymcpserver/src/main/java/com/eg/mcp/providers/others/StoreMcpCompleteProvider.java
@@ -4,47 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.stereotype.Service;
-
 import com.eg.mcp.utils.CountryPromptDatabase;
 import com.logaritex.mcp.annotation.McpComplete;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class StoreMcpCompleteProvider {
 
-    private CountryPromptDatabase countryPromptDatabase;
+	private CountryPromptDatabase countryPromptDatabase;
 
-    public StoreMcpCompleteProvider(CountryPromptDatabase countryPromptDatabase) {
-    	this.countryPromptDatabase = countryPromptDatabase;
-    }
+	public StoreMcpCompleteProvider(CountryPromptDatabase countryPromptDatabase) {
+		this.countryPromptDatabase = countryPromptDatabase;
+	}
 
-   
-  
-    
-
-	
-    
-    @McpComplete(prompt = "country-status")
+	@McpComplete(prompt = "country-status")
 	public List<String> completeCountryname(String countryPrefix) {
-    	if (countryPrefix == null || countryPrefix.isBlank()) {
-    	    return List.of("Enter a country");
-    	}
-		String prefix = countryPrefix.toLowerCase();
-		
-
-		
-		
-		Set<String> countries = countryPromptDatabase.getCountryDatabase().get(prefix);
-		if(countries==null || countries.size()==0)
-		{
-			return List.of(countryPrefix +" does not resolve to any country name or its begining");
+		if (countryPrefix == null || countryPrefix.isBlank()) {
+			return List.of("Enter a country");
 		}
-
+		String prefix = countryPrefix.toLowerCase();
+		Set<String> countries = countryPromptDatabase.getCountryDatabase().get(prefix);
+		if (countries == null || countries.size() == 0) {
+			return List.of(countryPrefix + " does not resolve to any country name or its begining");
+		}
 		return new ArrayList<>(countries);
 	}
 
-   
-    
-
-    
 }
