cb59647d7ddeb9fc924d2ee6a365b1f2b39b09c1
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2025-07-27 11:58:56 +0200

    remove unused references of IOException. Use default  ObjectMapper, managed by spring

--- mymcpserver/src/main/java/com/eg/mcp/providers/tools/StoreResourceNowToolsProvider.java
+++ mymcpserver/src/main/java/com/eg/mcp/providers/tools/StoreResourceNowToolsProvider.java
@@ -2,12 +2,6 @@ package com.eg.mcp.providers.tools;
 
 import java.io.IOException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.ai.tool.annotation.Tool;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 import com.eg.mcp.models.Cart;
 import com.eg.mcp.models.Order;
 import com.eg.mcp.models.SportsItem;
@@ -15,6 +9,12 @@ import com.eg.mcp.models.State;
 import com.eg.mcp.utils.MarkdownMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 /*
  * Initially was planning of making this a class for Resources and not Tools.
@@ -23,29 +23,34 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 @Service
 public class StoreResourceNowToolsProvider {
-	private static final Logger logger=LoggerFactory.getLogger(StoreResourceNowToolsProvider.class);
-	@Value("${server.port:8080}")
-	private int serverPort = 8080;
-
-	@Value("${mine.mcpurl:false}")
-	private boolean mineMcpurl = false;
-	private final ObjectMapper jsonMapper;
-	private final XmlMapper xmlMapper;
-	private final MarkdownMapper markdownMapper;
-	private State state;
-
-	public StoreResourceNowToolsProvider(State state) {
-		super();
-		this.state = state;
-		this.jsonMapper = new ObjectMapper();
-		this.xmlMapper = new XmlMapper(); // for XML
-		this.markdownMapper = new MarkdownMapper();
-	}
+	
+	private static final Logger logger = LoggerFactory.getLogger(StoreResourceNowToolsProvider.class);
 
 	private static final String RETURNS = "Returns the URL of the image of the ";
 
-	@Tool(name = "get_tennis_ball_image", description = RETURNS + "tennis ball")
+	@Value("${server.port:8080}")
+	private int serverPort;
 
+	@Value("${mine.mcpurl:false}")
+	private boolean mineMcpurl = false;
+
+	private final ObjectMapper jsonMapper;
+
+	private final XmlMapper xmlMapper;
+
+	private final MarkdownMapper markdownMapper;
+
+	private final State state;
+
+	public StoreResourceNowToolsProvider(ObjectMapper jsonMapper, XmlMapper xmlMapper, State state) {
+		super();
+		this.jsonMapper = jsonMapper;
+		this.xmlMapper = xmlMapper;
+		this.state = state;
+		this.markdownMapper = new MarkdownMapper();
+	}
+
+	@Tool(name = "get_tennis_ball_image", description = RETURNS + "tennis ball")
 	public String getTennisBallImage() {
 		return mduri(SportsItem.TENNIS_BALL);
 	}
@@ -66,59 +71,37 @@ public class StoreResourceNowToolsProvider {
 	}
 
 	@Tool(name = "get_cart_content_in_json", description = "get cart content formatted in json")
-
 	public String cartjson() throws IOException {
-
 		Cart cart = state.toCart();
-		String content = jsonMapper.writeValueAsString(cart);
-		return content;
+		return jsonMapper.writeValueAsString(cart);
 	}
 
 	@Tool(name = "get_cart_content_in_xml", description = "get cart content formatted in xml")
-
 	public String cartxml() throws IOException {
-
 		Cart cart = state.toCart();
-
-		String content = xmlMapper.writeValueAsString(cart);
-		return content;
-
+		return xmlMapper.writeValueAsString(cart);
 	}
 
 	@Tool(name = "get_cart_content_in_markdown", description = "get cart content formatted in markdown")
-
-	public String cartmd() throws IOException {
-
+	public String cartmd() {
 		Cart cart = state.toCart();
-
-		String content = markdownMapper.writeValueAsString(cart);
-		return content;
-
+		return markdownMapper.writeValueAsString(cart);
 	}
-	
+
 	@Tool(name = "get_last_order_content_in_json", description = "get last order content formatted in json")
-
 	public String lastorderjson() throws IOException {
-
 		Order lastOrder = state.getLastOrder();
-		String content = jsonMapper.writeValueAsString(lastOrder);
-		return content;
+		return jsonMapper.writeValueAsString(lastOrder);
 	}
 
 	@Tool(name = "get_last_order_content_in_xml", description = "get last order content formatted in xml")
-
 	public String lastorderxml() throws IOException {
-
 		Order lastOrder = state.getLastOrder();
-
-		String content = xmlMapper.writeValueAsString(lastOrder);
-		return content;
-
+		return xmlMapper.writeValueAsString(lastOrder);
 	}
 
 	@Tool(name = "get_last_order_content_in_markdown", description = "get last order content formatted in markdown")
-
-	public String lastordermd() throws IOException {
+	public String lastordermd() {
 		logger.debug("Entered lastordermd");
 		Order lastOrder = state.getLastOrder();
 		logger.debug("Got lastorder");
@@ -128,11 +111,8 @@ public class StoreResourceNowToolsProvider {
 		return content;
 
 	}
-	
-	
 
 	private String mduri(SportsItem sportsItem) {
-
 		return mineMcpurl ? "mcp://resource/" + sportsItem.touri()
 				: "http://localhost:" + serverPort + "/images/" + sportsItem.touri();
 	}
