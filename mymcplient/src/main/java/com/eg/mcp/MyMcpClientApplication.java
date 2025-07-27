e52ed16930c2ea94d01250f7f05f600f8c273f36
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2025-07-27 10:33:27 +0200

    Remove empty break lines / code formatting and review

--- mymcplient/src/main/java/com/eg/mcp/MyMcpClientApplication.java
+++ mymcplient/src/main/java/com/eg/mcp/MyMcpClientApplication.java
@@ -3,12 +3,6 @@ package com.eg.mcp;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-
 import io.modelcontextprotocol.client.McpSyncClient;
 import io.modelcontextprotocol.spec.McpSchema.CompleteRequest;
 import io.modelcontextprotocol.spec.McpSchema.CompleteResult;
@@ -20,7 +14,13 @@ import io.modelcontextprotocol.spec.McpSchema.ListToolsResult;
 import io.modelcontextprotocol.spec.McpSchema.PromptReference;
 import io.modelcontextprotocol.spec.McpSchema.ReadResourceRequest;
 import io.modelcontextprotocol.spec.McpSchema.ReadResourceResult;
- 
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class MyMcpClientApplication {
 
@@ -31,50 +31,40 @@ public class MyMcpClientApplication {
 
 	@Bean
 	public CommandLineRunner demo(List<McpSyncClient> mcpSyncClients, ConfigurableApplicationContext context) {
-		
 		return args -> {
-			
-			
-			if(mcpSyncClients.size()==1)
-			{
-				McpSyncClient client = mcpSyncClients.get(0);
-				
+			if (mcpSyncClients.size() == 1) {
+				McpSyncClient client = mcpSyncClients.getFirst();
 				ListToolsResult toolsList = client.listTools();
 				System.out.println("Available Tools = " + toolsList);
-				toolsList.tools().stream().forEach(tool -> {
+				toolsList.tools().forEach(tool -> {
 					System.out.println("Tool: " + tool.name() + ", description: " + tool.description() + ", schema: "
 							+ tool.inputSchema());
 				});
-				
+
 				ListResourcesResult listResources = client.listResources();
-				listResources.resources().forEach(resource->{
-					System.out.println("Resource: " +resource.name()+", "+resource.uri()+", "+resource.description());
+				listResources.resources().forEach(resource -> {
+					System.out.println("Resource: " + resource.name() + ", " + resource.uri() + ", " + resource.description());
 				});
 				ReadResourceResult resource = client.readResource(new ReadResourceRequest("mcp://brandz/store/rules"));
-			
 				System.out.println("result = " + resource);
-				
+
 				ListPromptsResult listPrompts = client.listPrompts();
-				listPrompts.prompts().forEach(prompt->{
-					System.out.println("Prompt: " +prompt.name()+", "+prompt.description());
+				listPrompts.prompts().forEach(prompt -> {
+					System.out.println("Prompt: " + prompt.name() + ", " + prompt.description());
 				});
 				GetPromptResult prompt = client.getPrompt(
 						new GetPromptRequest("brandz-greeting", Map.of("name", "Doe")));
 				System.out.println("result = " + prompt);
-				
+
 				// Completions
 				CompleteResult completion = client.completeCompletion(new CompleteRequest(new PromptReference("country-status"),
 						new CompleteRequest.CompleteArgument("countryName", "a")));
-			
 				System.out.println("Completion = " + completion);
-				
+
 				context.close();
-			
 			}
-			
 
 		};
 	}
 
-	
 }
